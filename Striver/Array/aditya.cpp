7993a6efdece26b331c5ffa83d5c8b96fc7e4905
Aditya <adixmeena@gmail.com>
2026-05-24 17:31:44 +0530

    feat: initial commit - 100 Days of DSA

---
+++ Striver/Array/aditya.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter your Name" << endl;
+
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << endl;
+
+    return 0;
+}
