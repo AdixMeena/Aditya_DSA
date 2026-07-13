55e31f1bb351597966c9c4d8809cdf0400f897a4
Aditya <adixmeena@gmail.com>
2026-07-13 23:19:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (76.91%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> result;
+        
+        for (int len = 2; len <= 9; len++) {
+            for (int start = 1; start + len - 1 <= 9; start++) {
+                int num = 0;
+                for (int d = start; d < start + len; d++) {
+                    num = num * 10 + d;
+                }
+                if (num >= low && num <= high) {
+                    result.push_back(num);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
